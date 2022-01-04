67531475f976a8570f1126bb8b289d6575597f2a
19037908 <moraga.hermosilla@hotmail.com>
2022-01-04 14:43:36 -0300

    fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(passport.initialize())
 app.use('/api', Router)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'))
+    app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname + '/client/build/index.html'))
     })
